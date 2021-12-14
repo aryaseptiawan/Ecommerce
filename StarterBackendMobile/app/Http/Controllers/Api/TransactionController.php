bf183627454d65927b0031dc09f4abb68c4682e3
brianadam18 <brianadam18@icloud.com>
2021-12-14 16:53:11 +0700

    fix controller & add app blade

--- StarterBackendMobile/app/Http/Controllers/Api/TransactionController.php
+++ StarterBackendMobile/app/Http/Controllers/Api/TransactionController.php
@@ -1,74 +1,81 @@
 <?php
 
-namespace App\Http\Controllers\Api;
+namespace App\Http\Controllers\API;
 
+use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\StoreTransactionRequest;
-use App\Http\Resources\TransactionResource;
 use App\Models\Transaction;
+use App\Models\TransactionItem;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TransactionController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function all(Request $request)
     {
-        return TransactionResource::collection(
-            Transaction::with('category')->get());
+        $id = $request->input('id');
+        $limit = $request->input('limit', 6);
+        $status = $request->input('status');
+
+        if($id)
+        {
+            $transaction = Transaction::with(['items.product'])->find($id);
+
+            if($transaction)
+                return ResponseFormatter::success(
+                    $transaction,
+                    'Data transaksi berhasil diambil'
+                );
+            else
+                return ResponseFormatter::error(
+                    null,
+                    'Data transaksi tidak ada',
+                    404
+                );
+        }
+
+        $transaction = Transaction::with(['items.product'])->where('users_id', Auth::user()->id);
+
+        if($status)
+            $transaction->where('status', $status);
+
+        return ResponseFormatter::success(
+            $transaction->paginate($limit),
+            'Data list transaksi berhasil diambil'
+        );
     }
 
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function store(StoreTransactionRequest $request)
+    public function checkout(Request $request)
     {
-        $transaction = auth()->user()->transactions()->create($request->validated());
+        $request->validate([
+            'items' => 'required|array',
+            'items.*.id' => 'exists:products,id',
+            'total_price' => 'required',
+            'shipping_price' => 'required',
+            'status' => 'required|in:PENDING,SUCCESS,CANCELLED,FAILED,SHIPPING,SHIPPED',
+        ]);
 
-        return new TransactionResource($transaction);
-    }
+        $transaction = Transaction::create([
+            'users_id' => Auth::user()->id,
+            'address' => $request->address,
+            'total_price' => $request->total_price,
+            'shipping_price' => $request->shipping_price,
+            'status' => $request->status
+        ]);
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Transaction  $transaction
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Transaction $transaction)
-    {
-        return new TransactionResource($transaction);
-    }
+        foreach ($request->items as $product) {
+            TransactionItem::create([
+                'users_id' => Auth::user()->id,
+                'products_id' => $product['id'],
+                'transactions_id' => $transaction->id,
+                'quantity' => $product['quantity']
+            ]);
+        }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Transaction  $transaction
-     * @return \Illuminate\Http\Response
-     */
-    public function update(StoreTransactionRequest $request, Transaction $transaction)
-    {
-        $transaction->update($request->validated());
-
-        return new TransactionResource($transaction);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Transaction  $transaction
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Transaction $transaction)
-    {
-        $transaction->delete();
-
-        return response()->noContent();
+        return ResponseFormatter::success($transaction->load('items.product'), 'Transaksi berhasil');
     }
 }
